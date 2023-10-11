2c3c8338cd66eaa929a636c64a8b2735bf11ab76
Ricardo Calix <rcalix1@gmail.com>
2023-10-11 18:46:52 -0500

    Update index.js

--- DeployToProduction/WineQuality/XGBoost/index.js
+++ DeployToProduction/WineQuality/XGBoost/index.js
@@ -16,13 +16,15 @@ async function runExample() {
      x[9] = document.getElementById('box10').value;
      x[10] = document.getElementById('box11').value;
 
-    let tensorX = new onnx.Tensor(x, 'float32', [1, 11]);
+    let tensorX = new ort.Tensor('float32', x, [1, 11] );
+    let feeds = {float_input: tensorX};
 
-    let session = new onnx.InferenceSession();
+    let session = await ort.InferenceSession.create('xgboost_WineQuality_ort.onnx');
+    
+   let result = await session.run(feeds);
+   let outputData = result.variable.data;
 
-    await session.loadModel("./DLnet_WineData.onnx");
-    let outputMap = await session.run([tensorX]);
-    let outputData = outputMap.get('output1');
+  outputData = parseFloat(outputData).toFixed(2)
 
    let predictions = document.getElementById('predictions');
 
@@ -30,7 +32,7 @@ async function runExample() {
    <table>
      <tr>
        <td>  Rating of Wine Quality  </td>
-       <td id="td0">  ${outputData.data[0].toFixed(2)}  </td>
+       <td id="td0">  ${outputData}  </td>
      </tr>
   </table>`;
     
