63d9433b150f9f05b5d65af31b4880c1fe5e7895
Ricardo Calix <rcalix1@gmail.com>
2023-10-16 19:01:12 -0500

    Update index.js

--- DeployToProduction/Sec1WineQuality/NN/index.js
+++ DeployToProduction/Sec1WineQuality/NN/index.js
@@ -1,6 +1,31 @@
 
 async function runExample() {
 
+  var x = [];
+
+  x[0] = document.getElementById('box1').value;
+  x[1] = document.getElementById('box2').value;
+  x[2] = document.getElementById('box3').value;
+  x[3] = document.getElementById('box4').value;
+  x[4] = document.getElementById('box5').value;
+  x[5] = document.getElementById('box6').value;
+  x[6] = document.getElementById('box7').value;
+  x[7] = document.getElementById('box8').value;
+  x[8] = document.getElementById('box9').value;
+  x[9] = document.getElementById('box10').value;
+  x[10] = document.getElementById('box11').value;
+
+  let tensorX = new onnx.Tensor(x, 'float32', [1, 11]);
+
+  let session = new onnx.InferenceSession();
+
+  await session.loadModel("DLnet_WineData.onnx");
+  let outputMap = await session.run([tensorX]);
+  let outputData = outputMap.get('output1');
+
+  
 
 
+
+  
 }
